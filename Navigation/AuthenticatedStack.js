a375d7cb253d4596fee8231f28e7f2ba72cd91e4
EmilFunhammar <emil.funhammar@gmail.com>
2021-01-31 13:35:55 +0100

    workout session screen

--- Navigation/AuthenticatedStack.js
+++ Navigation/AuthenticatedStack.js
@@ -3,15 +3,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Workout from '../screens/choose-workout';
 import Create_Workout from '../screens/create-workout';
 import Utveckling from '../screens/utveckling';
+import WorkoutSession from '../screens/workout-session';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Stack.Navigator initialRouteName="Create_Workout">
+    <Stack.Navigator initialRouteName="Workout_session">
       <Stack.Screen name="U" component={Utveckling} />
-      <Stack.Screen name="Choose_Workout" component={Workout} />
       <Stack.Screen name="Create_Workout" component={Create_Workout} />
+      <Stack.Screen name="Choose_Workout" component={Workout} />
+      <Stack.Screen name="Workout_session" component={WorkoutSession} />
     </Stack.Navigator>
   );
 }
---
+++ screens/workout-session.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+export default function WorkoutSession() {
+  // fetch the workout
+  const testArray = [
+    {
+      id: 1,
+      sets: 0,
+      reps: 8,
+      exercise: 'Biceps',
+    },
+    {
+      id: 2,
+      sets: 3,
+      reps: 3,
+      workout: 'legs',
+    },
+    {
+      id: 3,
+      sets: 4,
+      reps: 3,
+      workout: 'chest',
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{ backgroundColor: 'gray', width: '100%' }}
+        data={testArray}
+        renderItem={({ item, index }) => (
+          <ExerciseItem
+            exercise={item.exercise}
+            sets={item.sets}
+            reps={item.reps}
+          />
+        )}
+        ItemSeparatorComponent={() => {
+          return (
+            <View
+              style={{
+                backgroundColor: 'transparent',
+                width: '100%',
+                height: 10,
+              }}
+            ></View>
+          );
+        }}
+      />
+    </View>
+  );
+}
+
+const ExerciseItem = ({ exercise, sets, reps }) => {
+  return (
+    <View style={styles.exerciseItem}>
+      <Text style={styles.exercise}>{exercise}</Text>
+      <RenderUsers sets={sets} reps={reps} />
+      <RenderUsers sets={sets} reps={reps} />
+      <RenderUsers sets={sets} reps={reps} />
+    </View>
+  );
+};
+
+const RenderUsers = ({ exercise, sets, reps }) => {
+  return (
+    <View style={styles.setsAndReps}>
+      <View style={styles.users}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <View style={styles.checkBox}></View>
+          <Text> Player1</Text>
+        </View>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <NegativSignView />
+        <View>
+          <Text>sets</Text>
+          <Text>3/{sets}</Text>
+        </View>
+        <PlusSignView />
+        <View style={{ flexDirection: 'row' }}>
+          <NegativSignView />
+          <View>
+            <Text>reps</Text>
+            <Text>3/{reps}</Text>
+          </View>
+          <PlusSignView />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const PlusSignView = () => {
+  return (
+    <View style={styles.add_and_remove}>
+      <Text
+        style={styles.add_and_remove_text}
+        onPress={() => console.log('plus sign pressed')}
+      >
+        +
+      </Text>
+    </View>
+  );
+};
+
+const NegativSignView = () => {
+  return (
+    <View style={styles.add_and_remove}>
+      <Text
+        style={styles.add_and_remove_text}
+        onPress={() => console.log('negative sign pressed')}
+      >
+        -
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  exerciseItem: {
+    backgroundColor: '#3F7134',
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10,
+    borderTopEndRadius: 5,
+    borderTopLeftRadius: 5,
+  },
+  exercise: {
+    marginLeft: 10,
+    marginTop: 10,
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+
+  setsAndReps: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 3,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+  setsAndRepsNumbers: {},
+  users: {},
+  add_and_remove: {
+    //backgroundColor: '#3F7134',
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    width: 30,
+  },
+  add_and_remove_text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  checkBox: {
+    backgroundColor: 'red',
+    height: 20,
+    width: 20,
+    borderRadius: 20,
+    marginLeft: 5,
+  },
+});
